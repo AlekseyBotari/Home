c1e2a8e7a732fb90614439ea08139e8440db9d3f
Botari <hydrokinetics745@gmail.com>
2022-10-25 18:55:49 +0300

    Add homework_2

---
+++ main.py
@@ -0,0 +1,17 @@
+print('Hello world')  # output 'Hello world'
+
+print('')
+
+part_1 = '*****'  # output '*****'
+
+print(str(part_1)) # output square 5х5х with '*****'
+
+Part_2 = """
+#####
+#####
+#####
+#####
+#####
+"""
+
+print(str(Part_2))
